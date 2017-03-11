e3bdb0bd6cbb9bca6d59bed7d734feed91e0d0f8
JunyanZhao <zhaojunyan@zhaojunyandeMacBook-Pro.local>
2017-03-12 07:37:15 +0800

    Initialize
    20170312 zhaojunyan

---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  R01C00
+//
+//  Created by 赵军岩 on 2017/3/12.
+//  Copyright © 2017年 赵军岩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! The program is starting! \n";
+    return 0;
+}
